a78e3754b253be3a50657fbb0d66e700f700f0af
Guanshu Tao <guanshu@ualberta.ca>
2020-11-20 23:13:45 -0700

    add main function

--- demo.py
+++ demo.py
@@ -8,10 +8,16 @@ Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[--arg4=<arg4>]   Takes any value (this is a required positional argument)
 """
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt)
-print(type(opt))
-print(opt['--arg4'])
+
+def main(arg1, arg2, arg3, arg4):
+  print(opt)
+  print(type(opt))
+  print(opt['--arg4'])
+
+if __name__ == "__main__": 
+ main(opt["<arg1>"], opt["--arg2"], opt["--arg3"], opt["--arg4"]) 
